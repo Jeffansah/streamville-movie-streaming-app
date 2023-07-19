00315832027536eb73fd544e412310ab4148f4fb
Jeffrey Owusu Ansah <jeffansah13@gmail.com>
2023-07-19 15:39:05 +0000

    Initial Commit

---
+++ components/Header.js
@@ -0,0 +1,29 @@
+import HeaderItem from "./HeaderItem";
+import {
+  BadgeCheckIcon,
+  CollectionIcon,
+  HomeIcon,
+  LightningBoltIcon,
+  SearchIcon,
+  UserIcon,
+} from "@heroicons/react/outline";
+
+const Header = () => {
+  return (
+    <header className="flex flex-col sm:flex-row m-5 items-center justify-between h-auto">
+      <div className="flex flex-grow justify-evenly max-w-2xl">
+        <HeaderItem title="HOME" Icon={HomeIcon} />
+        <HeaderItem title="TRENDING" Icon={LightningBoltIcon} />
+        <HeaderItem title="VERIFIED" Icon={BadgeCheckIcon} />
+        <HeaderItem title="COLLECTIONS" Icon={CollectionIcon} />
+        <HeaderItem title="SEARCH" Icon={SearchIcon} />
+        <HeaderItem title="ACCOUNT" Icon={UserIcon} />
+      </div>
+      <div>
+        <h1 className="text-4xl font-extrabold text-white">StreamVille</h1>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
